d64a7df1ec4ff64f95c6017a5808ce29a99a6025
maemo <maemo@maemo-TomXue>
2011-10-31 14:59:22 +0000

    SIGIO and fcntl applied, basic FASYNC trial

---
+++ trial_SIGIO_FASYNC.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <signal.h>
+#include <fcntl.h>
+ 
+int gotdata=0;
+void sighandler(int signo)
+{
+    if (signo==SIGIO)
+        gotdata++;
+    return;
+}
+ 
+char buffer[4096];
+ 
+int main(int argc, char **argv)
+{
+    int count;
+    struct sigaction action;
+ 
+    memset(&action, 0, sizeof(action));
+    action.sa_handler = sighandler;
+    action.sa_flags = 0;
+
+    sigaction(SIGIO, &action, NULL);  //SIGIO: I/O now possible
+
+    fcntl(STDIN_FILENO, F_SETOWN, getpid());  //F_SETOWN: Get owner (process receiving SIGIO); the owner process if current process
+    fcntl(STDIN_FILENO, F_SETFL, fcntl(STDIN_FILENO, F_GETFL) | FASYNC);
+ 
+    while(1) {
+        /* this only returns if a signal arrives */
+        sleep(86400); /* one day */
+
+        if (!gotdata)
+            continue;
+        count=read(0, buffer, 4096);
+        /* buggy: if avail data is more than 4kbytes... */
+        write(1,buffer,count);
+        gotdata=0;
+    }
+}
